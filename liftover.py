72fb1ac90d510f238ee4f6b13cadc4931861d141
beml201 <75611311+beml201@users.noreply.github.com>
2025-01-06 18:31:33 +0000

    Fixed double line bug and missing chr23 bug

--- liftover.py
+++ liftover.py
@@ -7,66 +7,92 @@ parser = argparse.ArgumentParser()
 
 # Add arguments
 parser.add_argument('chr', help="Chromosome column number (0 index)", type=int)
-parser.add_argument('pos', help="position column number (0 index)", type=int)
+parser.add_argument('pos', help="Position column number (0 index)", type=int)
 parser.add_argument('build_from', help="Which build to convert from (eg b38)", type=str)
 parser.add_argument('build_to', help="Which build to convert to (eg b38)",type=str)
 parser.add_argument('file', help="File to Convert")
 parser.add_argument("--header", help="include if file has a header (Default no-header)", action=argparse.BooleanOptionalAction)
-parser.add_argument('--delim', help="Delimiter of input file")
-parser.add_argument('--delim_out', help="Delimiter to use when outputting file (Default tab)")
+parser.add_argument('--delim', default="\t", help="Delimiter of input file")
+parser.add_argument('--delim_out', help="Delimiter to use when outputting file (Default: same as delim)")
 parser.add_argument('--out', help='file output name')
 
 args = parser.parse_args()
 print(args)
 
-# Get liftover chain
-build_dictionary = {
-    'b38':'hg38',
-    'b37':'hg19',
-    '38':'hg38',
-    '37':'hg19'
-}
-if args.build_from in build_dictionary.keys():
-    args.build_from = build_dictionary[args.build_from]
-if args.build_to in build_dictionary.keys():
-    args.build_to = build_dictionary[args.build_to]
-lo = LiftOver(args.build_from, args.build_to)
+def build_dictionary(build_identifier):
+    # Get liftover chain
+    known_builds = {
+        'b38':'hg38',
+        'b37':'hg19',
+        '38':'hg38',
+        '37':'hg19'
+    }
+    if build_identifier in known_builds.keys():
+        return known_builds[build_identifier]
+    else:
+        return build_identifier
 
-# If out was not input, make a new output name
+def format_chromosome(chromosome: str):
+    chromosome = chromosome.replace('23', 'X').replace('24','Y')
+    if chromosome.startswith('chr'):
+        return chromosome
+    else:
+        return f"chr{chromosome}"
+    
+def unformat_chromosome(original_chrom: str, new_chrom: str):
+    out = new_chrom
+    if not original_chrom.startswith('chr'):
+        out = new_chrom.removeprefix('chr')
+    if 'X' in new_chrom and '23' in original_chrom:
+        out = out.replace('X', '23')
+    if 'Y' in new_chrom and '24' in original_chrom:
+        out = out.replace('Y', '24')
+    return out
+
+# Update build numbers
+args.build_from = build_dictionary(args.build_from)
+args.build_to = build_dictionary(args.build_to)
+
+# Update output file name if necessary
 if args.out is None:
-    args.out = args.build_to + '_converted_' + args.file
-    print('No output file name given, writing to: '+args.out)
+    args.out = f"{args.file}.{args.build_to}"
+    print(f"No output file name given, writing to: {args.out}")
+
+# Update output delimiter if necessary
 if args.delim_out is None:
-    args.delim_out = '\t'
+    args.delim_out = args.delim
+
+# Construct the liftover chain object
+lo = LiftOver(args.build_from, args.build_to)
 
 # Open files 1 and 2 and read/write them
 file1 = open(args.file, 'r')
 file2 = open(args.out, 'w+')
 
+# Add the header back in if necessary
 if args.header:
-    file2.write(file1.readline())
+    file2.write(file1.readline().replace(args.delim, args.delim_out))
 
 line_count = 0
 missing_count = 0
 for line in file1:
-    if args.delim is None:
-        out = line.split()
-    else:
-        out = line.split(args.delim)
+    out = line.strip().split(args.delim)
         
-    if 'chr' not in out[args.chr]:
-        out[args.chr] = 'chr' + str(out[args.chr])
-        chr_replace = ''
-    else:
-        chr_replace = 'chr'
-    new_pos = lo.convert_coordinate(out[args.chr], int(out[args.pos]))
+    #if 'chr' not in out[args.chr]:
+    #    out[args.chr] = 'chr' + str(out[args.chr])
+    #    chr_replace = ''
+    #else:
+    #    chr_replace = 'chr'
+    new_pos = lo.convert_coordinate(format_chromosome(out[args.chr]), int(out[args.pos]))
     
-    if len(new_pos) == 0:
+    if new_pos is None:
+        out[args.pos] = 'LIFTOVER_ERROR'
+    elif len(new_pos) == 0:
         out[args.pos] = 'NA'
-        missing_count +=1
+        missing_count += 1
     else:
+        out[args.chr] = unformat_chromosome(out[args.chr], new_pos[0][0])
         out[args.pos] = new_pos[0][1]
-        out[args.chr] = new_pos[0][0].replace('chr', chr_replace)
         
     out = [str(x) for x in out]
     out = args.delim_out.join(out)
